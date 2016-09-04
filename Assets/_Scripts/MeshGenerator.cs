755e66f4c9943b88b5e4c943ec9964fb10625436
Bermuda_UT <maxisbac@gmail.com>
2016-09-04 20:48:28 +1000

    Mesh normal optimization and update

--- Assets/_Scripts/MeshGenerator.cs
+++ Assets/_Scripts/MeshGenerator.cs
@@ -4,14 +4,15 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Security.Policy;
+using System.Xml;
 
 public class MeshGenerator : MonoBehaviour {
-    private static int vertexLimit = 64000;
+    private static int vertexLimit = 60000;
 
     public static Mesh[] PlaneFromArray(float[][] t, float xSize, float zSize, float minHeight, float maxHeight, bool useCustom=false, Vector3 normal = new Vector3(), Color c = new Color()) {
         int total = t.Length * t[0].Length;
         int meshCount = (int) (total/vertexLimit) + 1;
-        Mesh[] mArr= new Mesh[meshCount+1];
+        Mesh[] mArr = new Mesh[meshCount + 1];
 
         int breakLeap = (int) (t.Length/meshCount);
         int breakIndex = 0;
@@ -23,16 +24,21 @@ public class MeshGenerator : MonoBehaviour {
 
             Mesh m = mArr[z];
             m.name = "GeneratedMesh_" + z;
+            Vector3[][] n = calcNormals(t, xSize, zSize);
 
             List<int> tList = new List<int>();
             List<Color> cList = new List<Color>();
             List<Vector3> vList = new List<Vector3>();
+            List<Vector3> nList = new List<Vector3>();
 
             // add verticies and triangles
             for (int k = 0; i < breakIndex; i++, k++) {
                 for (int j = 0; j < t[i].Length; j++) {
                     // verticies
                     vList.Add(new Vector3(j*xSize, t[i][j], i*zSize));
+
+                    // normals
+                    nList.Add(n[i][j]);
 
                     // triangles
                     if (j < t[i].Length - 1 && i < breakIndex - 1) {
@@ -59,12 +65,74 @@ public class MeshGenerator : MonoBehaviour {
             m.vertices = vList.ToArray();
             m.colors = cList.ToArray();
             m.triangles = tList.ToArray();
-
-            // add normals
-            m.RecalculateNormals();
+            m.normals = nList.ToArray();
         }
 
         return mArr;
+    }
+
+    public static Vector3[][] calcNormals(float[][] t, float xSize, float zSize) {
+        Vector3[][] n = new Vector3[t.Length][];
+
+        for(int i = 0; i < t.Length; i++) {
+            n[i] = new Vector3[t[i].Length];
+            for (int j = 0; j < t[i].Length; j++) {
+                Vector3 a = new Vector3();
+                Vector3 curr = new Vector3(j*xSize, t[i][j], i*zSize);
+                int c = 0;
+
+                if (i > 0) {
+                    if (j > 0) {
+                        a += Vector3.Cross(
+                            new Vector3(j*xSize, t[i-1][j], (i-1)*zSize) - curr,
+                            new Vector3((j-1)*xSize, t[i][j-1], i*zSize) - curr
+                        );
+                        c++;
+                    } 
+                    if(j < t[i].Length - 1) {
+                        a += Vector3.Cross(
+                            new Vector3((j+1)*xSize, t[i-1][j+1], (i-1)*zSize) - curr,
+                            new Vector3(j*xSize, t[i-1][j], (i-1)*zSize) - curr
+                        );
+
+                        a += Vector3.Cross(
+                            new Vector3((j+1)*xSize, t[i][j+1], i*zSize) - curr,
+                            new Vector3((j+1)*xSize, t[i-1][j+1], (i-1)*zSize) - curr
+                        );
+
+                        c+=2;
+                    }
+                }
+
+                if (i < t.Length - 1) {
+                    if (j > 0) {
+                        a += Vector3.Cross(
+                            new Vector3((j-1)*xSize, t[i+1][j-1], (i+1)*zSize) - curr,
+                            new Vector3(j*xSize, t[i+1][j], (i+1)*zSize) - curr
+                        );
+
+                        a += Vector3.Cross(
+                            new Vector3((j-1)*xSize, t[i][j-1], i*zSize) - curr,
+                            new Vector3((j-1)*xSize, t[i+1][j-1], (i+1)*zSize) - curr
+                        );
+
+                        c+=2;
+                    } 
+                    if(j < t[i].Length - 1) {
+                        a += Vector3.Cross(
+                            new Vector3(j*xSize, t[i+1][j], (i+1)*zSize) - curr,
+                            new Vector3((j+1)*xSize, t[i][j+1], i*zSize) - curr
+                        );
+                        c++;
+                    }
+                    
+                }
+
+                n[i][j] = (a / c).normalized;
+            }
+        }
+
+        return n;
     }
 
     public static Color HexToColor(string hex, byte alpha) {
@@ -79,18 +147,22 @@ public class MeshGenerator : MonoBehaviour {
 
         if (v.y <= minHeight/2)
             c = MeshGenerator.HexToColor("424242", 255);
-        if (v.y <= 2*minHeight/3)
+        else if (v.y <= 2*minHeight/3)
             c = MeshGenerator.HexToColor("9e9e9e", 255);
         else if(v.y <= 0f)
             c = MeshGenerator.HexToColor("6d4c41", 255);
+        else if(v.y <= maxHeight/25)
+            c = MeshGenerator.HexToColor("ffe57f", 255);
         else if(v.y <= maxHeight/4)
             c = MeshGenerator.HexToColor("689f38", 255);
         else if(v.y <= maxHeight/2)
             c = MeshGenerator.HexToColor("8bc34a", 255);
+        else if(v.y <= 5*maxHeight/8)
+            c = MeshGenerator.HexToColor("616161", 255);
         else if(v.y <= 3*maxHeight/4)
-            c = MeshGenerator.HexToColor("eeeeee", 255);
+            c = MeshGenerator.HexToColor("e1f5fe", 255);
         else
-            c = MeshGenerator.HexToColor("fafafa", 255);
+            c = MeshGenerator.HexToColor("eeeeee", 255);
 
         return c;
     }
